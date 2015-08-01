379d0ecf185bbd371e2dc446ff34f848239a2541
zaverden <zaverden@gmail.com>
2015-08-01 13:12:45 +0700

    init solution

---
+++ FknSamples/FknSamples/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace FknSamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var methods = typeof (HomeController).GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                .Where(m => !m.IsSpecialName)
+                .Where(m => m.DeclaringType == typeof(HomeController))
+                .Where(m => m.Name != "Index");
+            return View(methods.Select(m => m.Name));
+        }
+
+
+
+    }
+}
